c4b935e2a5ace69a761a63bc8e32aae23c947087
smaintos <william.mariamon@ynov.com>
2025-01-16 14:30:50 +0100

    start room 4

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
 const express = require('express');
+const http = require('http');
 const cors = require('cors');
-const playlistRoutes = require('./routes/playlist');
-const gameRoutes = require('./routes/game');
 
+// Import (ou pas) pour d’autres routes REST existantes
+const playlistRoutes = require('./routes/playlist');
+require('./socketGame');
 
 const app = express();
-const PORT = process.env.PORT || 5002;
-
 app.use(cors());
 app.use(express.json());
+
 app.use('/api', playlistRoutes);
-app.use('/api', gameRoutes);
 
+// On crée un serveur HTTP brut
+const server = http.createServer(app);
 
-app.listen(PORT, () => {
+// On plug Socket.io sur ce serveur
+const { Server } = require('socket.io');
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+const PORT = process.env.PORT || 5002;
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+// On exporte io ou on gère la logique Socket.io ici
+module.exports = { io };
