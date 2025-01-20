3f77ff0acee4963e69c18141965e86b4588ae93b
smaintos <william.mariamon@ynov.com>
2025-01-20 15:38:16 +0100

    yes bb

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
-const { Server } = require('socket.io');
 const initializeSocketIO = require('./services/socketService');
-
 
 // Import des routes
 const playlistRoutes = require('./routes/playlist');
@@ -18,21 +16,7 @@ app.use('/api', playlistRoutes);
 const server = http.createServer(app);
 
 // Initialisation de Socket.IO
-const io = initializeSocketIO(server);
-
-// Middleware pour convertir les requêtes HTTP en événements Socket.IO
-app.post('/socket.io/', (req, res) => {
-  const { type, payload } = req.body;
-  const socket = io.sockets.connected[req.headers['socket-id']];
-  
-  if (!socket) {
-    return res.status(400).json({ error: 'Socket not connected' });
-  }
-
-  socket.emit(type, payload, (response) => {
-    res.json(response);
-  });
-});
+initializeSocketIO(server);
 
 const PORT = process.env.PORT || 5002;
 server.listen(PORT, () => {
