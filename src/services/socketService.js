7180313d6b7cd8e1ba0d83c8f72f394367a262e7
smaintos <william.mariamon@ynov.com>
2025-01-25 15:56:23 +0100

    oui

--- src/services/socketService.js
+++ src/services/socketService.js
@@ -4,7 +4,10 @@ function initializeSocketIO(server) {
   const io = new Server(server, {
     cors: {
       origin: "*",
-    },
+      methods: ["GET", "POST"],
+      allowedHeaders: ["my-custom-header"],
+      credentials: true
+    }
   });
 
   require('../routes/socketGame')(io);
